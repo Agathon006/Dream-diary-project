79e65e474df25960de1be71205c41d2931e84c17
Agathon006 <agaton615@gmail.com>
2024-01-22 03:22:00 +0300

    Publishing project skeleton with all needed folders including webpack, babel, json-server and MVC pattern

---
+++ js/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+// import Model from './model/model.js'
+// import View from './view/view.js'
+// import Controller from './controller/controller.js'
+
+window.addEventListener('DOMContentLoaded', () => {
+    // new Controller(new View(), new Module()).init();
+});
