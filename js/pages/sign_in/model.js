88afb79b586059d4ca7e2e88cafe1688c6bfc148
Agathon006 <agaton615@gmail.com>
2024-01-25 08:36:29 +0300

    completed sign-in page with MVC

---
+++ js/pages/sign_in/model.js
@@ -0,0 +1,25 @@
+export default class Model {
+
+    isEmailOkay(emailInput) {
+        return emailInput.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    }
+
+    isPasswordOkay(passwordInput) {
+        return passwordInput.match(/^(?=.*[a-z])(?=.*[A-Z]).{6,200}$/);
+    }
+
+    getPromiseDbUsers() {
+        return fetch('http://localhost:3000/users')
+    }
+
+    createJwt(userData) {
+        const jwt = require('jsonwebtoken');
+        const payload = userData;
+        const secretKey = '8dshsdf8s3hfsdh8fshf8dhfs3hhfhfsh38fh';
+        const token = jwt.sign(payload, secretKey, { expiresIn: '24h' });
+
+        localStorage.token = token;
+        localStorage.secretKey = secretKey;
+    }
+
+}
