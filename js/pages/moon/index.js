d8cd7b10f8255754d7fa869501aea68f78befa23
Agathon006 <agaton615@gmail.com>
2024-01-25 12:07:02 +0300

    made start version of moon page

--- js/pages/moon/index.js
+++ js/pages/moon/index.js
@@ -1,4 +1,9 @@
 'use strict';
 
+import Model from './model.js'
+import View from './view.js'
+import Controller from './controller.js'
+
 window.addEventListener('DOMContentLoaded', () => {
+    new Controller(new View(), new Model()).init();
 });
