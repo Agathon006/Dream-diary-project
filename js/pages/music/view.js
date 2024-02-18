46ad9fb82358460615e96d3269cce2f0627e6470
Agathon006 <agaton615@gmail.com>
2024-02-18 23:48:33 +0300

    added ru translation to music page

---
+++ js/pages/music/view.js
@@ -0,0 +1,34 @@
+import i18next from 'i18next';
+
+export default class View {
+
+    translatePage() {
+        fetch('../dictionary.json')
+            .then(response => response.json())
+            .then(data => {
+                i18next.init({
+                    lng: 'ru',
+                    debug: false,
+                    resources: {
+                        ru: {
+                            translation: data
+                        }
+                    }
+                });
+
+                document.querySelector(`#header-link-home`).textContent = i18next.t('registered_header.home');
+                document.querySelector(`#header-link-time`).textContent = i18next.t('registered_header.moon');
+                document.querySelector(`#header-link-moon`).textContent = i18next.t('registered_header.time');
+                document.querySelector(`#header-link-music`).textContent = i18next.t('registered_header.music');
+                document.querySelector(`#header-link-profile`).textContent = i18next.t('registered_header.profile');
+                document.querySelector(`#header-link-sign-out`).textContent = i18next.t('registered_header.sign_out');
+
+                document.querySelector(`#music-player-title`).textContent = i18next.t('music.music_player_title');
+
+                document.querySelector(`#footer-plot`).textContent = i18next.t('footer.footer_plot');
+            })
+            .catch(error => {
+                console.error('Error loading JSON file:', error);
+            });
+    }
+}
